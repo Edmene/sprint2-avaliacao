a79b5a37bd79f400f4b08809da5e55379d7dcef3
leosantoos <58108641+odranoel1337@users.noreply.github.com>
2021-05-22 01:10:32 -0300

    criando rotas (post,put,delete) e metodos controller

--- 5/controller/MoneyTransactionController.js
+++ 5/controller/MoneyTransactionController.js
@@ -1,7 +1,7 @@
 const Money_transaction = require('../database/ModeloTabelaMoney_Transaction')
 
 module.exports = {
-    async index (req,res){
+    async listar (req,res){
         try{
             const money_transaction = await Money_transaction.findAll()
 
@@ -9,5 +9,45 @@ module.exports = {
         }catch(erro){
             return res.status(400).json({error:ValidationErrorItem.message})    
         }
+    },
+    async inserir(req, res) {
+        try {
+          const money_transaction = await Money_transaction.create(req.body);
+    
+          return res.json(money_transaction);
+        } catch (err) {
+          return res.status(400).json({ error: err.message });
+        }
+    },
+    async buscarId(req, res) {
+        try {
+          const money_transaction = await Money_transaction.findByPk(req.params.id);
+    
+          return res.json(money_transaction);
+        } catch (err) {
+          return res.status(400).json({ error: err.message });
+        }
+      },
+    async atualizar(req, res) {
+        try {
+          const money_transaction = await Money_transaction.findByPk(req.params.id);
+    
+          await money_transaction.update(req.body);
+    
+          return res.json({ money_transaction });
+        } catch (err) {
+          return res.status(400).json({ error: err.message });
+        }
+      },
+    async deletar(req, res) {
+        try {
+          const money_transaction = await Money_transaction.findByPk(req.params.id);
+    
+          await money_transaction.destroy();
+    
+          return res.json(money_transaction);
+        } catch (err) {
+          return res.status(400).json({ error: err.message });
+        }
     }
-}
+} 
--- 5/routes/index.js
+++ 5/routes/index.js
@@ -2,6 +2,10 @@ const roteador = require('express').Router()
 const money_transaction = require('../controller/MoneyTransactionController')
 
 
-roteador.get('/money_transaction',money_transaction.index)
+roteador.get('/money_transaction',money_transaction.listar)
+roteador.post('/money_transaction',money_transaction.inserir)
+roteador.get('/money_transaction/:id',money_transaction.buscarId)
+roteador.put('/money_transaction/:id',money_transaction.atualizar)
+roteador.delete('/money_transaction/:id',money_transaction.deletar)
 
 module.exports = roteador
