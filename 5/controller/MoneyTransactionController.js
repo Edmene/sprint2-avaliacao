cf76487ef2797483430719840c68182cae7afdb7
leosantoos <58108641+odranoel1337@users.noreply.github.com>
2021-05-22 10:50:47 -0300

    acrescentando metodos de busca por sender e receiver

--- 5/controller/MoneyTransactionController.js
+++ 5/controller/MoneyTransactionController.js
@@ -28,6 +28,28 @@ module.exports = {
           return res.status(400).json({ error: err.message });
         }
       },
+    async buscarSender(req, res) {
+        try {
+          const money_transaction = await Money_transaction.findOne({where:{sender:req.params.sender}});
+          if(money_transaction == null){
+            throw new Error("Sender not found!")
+          }
+          return res.json(money_transaction);
+        } catch (err) {
+          return res.status(400).json({ error: err.message });
+        }
+      },
+      async buscarReceiver(req, res) {
+        try {
+          const money_transaction = await Money_transaction.findOne({where:{receiver:req.params.receiver}});
+          if(money_transaction == null){
+            throw new Error("Sender not found!")
+          }
+          return res.json(money_transaction);
+        } catch (err) {
+          return res.status(400).json({ error: err.message });
+        }
+      },  
     async atualizar(req, res) {
         try {
           const money_transaction = await Money_transaction.findByPk(req.params.id);
--- 5/routes/index.js
+++ 5/routes/index.js
@@ -5,6 +5,8 @@ const money_transaction = require('../controller/MoneyTransactionController')
 roteador.get('/money_transaction',money_transaction.listar)
 roteador.post('/money_transaction',money_transaction.inserir)
 roteador.get('/money_transaction/:id',money_transaction.buscarId)
+roteador.get('/money_transaction/sender/:sender',money_transaction.buscarSender)
+roteador.get('/money_transaction/receiver/:receiver',money_transaction.buscarReceiver)
 roteador.put('/money_transaction/:id',money_transaction.atualizar)
 roteador.delete('/money_transaction/:id',money_transaction.deletar)
 
