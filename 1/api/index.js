e71e6c4083d862d1c1548f349ad78df0109e5394
leoaugusto02 <leonidasaugusto02@hotmail.com>
2021-05-22 21:53:00 -0300

    Montando a conexão com o banco de dados

--- 1/api/index.js
+++ 1/api/index.js
@@ -2,4 +2,7 @@ const express = require("express");
 
 const app = express();
 
+//o body-parser já vem nativo no express, então não é necessário usá-lo
+app.use(express.json());
+
 app.listen(3000, () => console.log("Estou no ar"));
